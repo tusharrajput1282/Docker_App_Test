9b3324a75f3090032dd9945ca4d2d2a7fed9c66d
tusharrajput1282 <graphicbusiness@gmail.com>
2025-05-21 22:58:02 +0530

    Docker-App-v1

---
+++ src/main/java/com/docker/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/message")
+	public String getMessage(){
+		return "Hello-World !!";
+	}
+
+}
